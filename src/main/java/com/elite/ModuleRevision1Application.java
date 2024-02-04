cf50da43efba9eec6cb455651a247ae88cff2151
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-02-04 09:15:23 +0100

    Initial commit

---
+++ src/main/java/com/elite/ModuleRevision1Application.java
@@ -0,0 +1,13 @@
+package com.elite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleRevision1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleRevision1Application.class, args);
+	}
+
+}
